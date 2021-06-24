68f67f9a9f390224bc263c17324c625ad8c0f838
Dhanush T <83498774+Dhanush-T@users.noreply.github.com>
2021-06-25 01:14:26 +0530

    Add files via upload

---
+++ store_MoM.py
@@ -0,0 +1,38 @@
+import os
+import mysql.connector
+
+mydb=mysql.connector.connect(host="localhost",user="Jay_Jay",password="password")
+mycursor=mydb.cursor()
+
+#Checking whether already databse exit or not, else creating a databse named store_MoMs
+mycursor.execute("CREATE DATABASE IF NOT EXISTS store_MoMs")
+
+#Checking whether Table names store_MoMs exits or not, else creating one
+mycursor.execute("SHOW TABLES")
+if "store_MoMs" not in mycursor:
+	mycursor.execute("CREATE TABLE store_MoMs (name VARCHAR(255) AUTO_INCREMENT PRIMARY KEY, dates VARCHAR(255), content TEXT)")
+
+
+os.chdir('/home')
+files=os.listdir('/home')
+
+#Inserting values into the table
+for f in files:
+	dir='/home/'+str(f)
+	os.chdir(dir)
+	text_files=os.listdir(dir)
+	for i in text_files:
+		if "_mom.txt" in i:
+			uname=str(f)
+			date=i[:10]
+			with open(i) as mom_file:
+				contents=mom_file.read()
+			sql="INSERT INTO store_MoMs (name, dates, content) VALUES (%s, %s, %s)"
+			val=(uname, date, contents)
+			mycursor.execute(sql, val)
+	mydb.commit()
+	os.chdir('/home')
+
+
+mydb.close()
+
