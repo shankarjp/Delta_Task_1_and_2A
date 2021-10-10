931cb8636fcb57af160f9c307b339a76ab9a74b5
Shankar JP <shankarjp12@outlook.com>
2021-10-10 22:40:28 +0530

    Files added

---
+++ README.md
@@ -0,0 +1,6 @@
+# Delta System Administration Task 1 and 2A
+
+## Commands
+```
+docker-compose up
+```
---
+++ get_log.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+URL='https://inductions.delta.nitt.edu/sysad-task-1-attendance.log'
+wget -N "$URL"
+chmod u+x sysad-task-1-attendance.log
+
+URL='https://inductions.delta.nitt.edu/sysad-task-1-future.txt'
+wget -N "$URL"
+chmod u+x sysad-task-1-future.txt
